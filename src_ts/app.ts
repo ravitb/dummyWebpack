6c95fa62ed2f11ad30c018c65c4f88a04f842e06
ravitb <ravit@rounds.com>
2015-04-20 15:52:47 +0300

    Removed unnecessary code

--- src_ts/app.ts
+++ src_ts/app.ts
@@ -1,10 +1,1 @@
 ///<reference path="testi.d.ts" />
-
-
-/*import ImageViewer = require('./imageViewer');
- import React = require('react/addons');*/
-
-interface State {
-	photoIndex?: number;
-	inputText?: string;
-}
