f7dac6a28b7c27fbf8b6d3f63c18322034d2411e
ravitb <ravit@rounds.com>
2015-04-20 14:55:47 +0300

    Tested typescript with webpack

---
+++ src_ts/app.ts
@@ -0,0 +1,10 @@
+///<reference path="testi.d.ts" />
+
+
+/*import ImageViewer = require('./imageViewer');
+ import React = require('react/addons');*/
+
+interface State {
+	photoIndex?: number;
+	inputText?: string;
+}
---
+++ src_ts/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	entry: './app.ts',
+	output: {
+		filename: 'bundle.js',
+		publicPath: '/build/'
+	},
+	resolve: {
+		extensions: ['', '.ts', '.js' ]
+	},
+	devtool: 'source-map',
+	module: {
+		loaders: [
+			{ test: /\.ts$/, loader: 'ts?sourceMap' }
+		]
+	}
+};
